ff7e1f6926906a7416c9c4672fd3b76575329e99
Hekk_Elek <horvath.kristof.sz@gmail.com>
2017-12-28 17:08:13 +0100

    First commit.

---
+++ src/bin-converter/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "types.h"
+#include "binconvert.h"
+
+FILE* frp, *fwp;
+
+U8 u8Sector[ 2352 ];
+U8 u8PitsnLands[ 2352 ];
+
+int main()
+{
+    U16 u16ReadBytes;
+
+    printf("CD binary converter test\n");
+
+    frp = fopen( "padtest.bin", "rb" );
+    fwp = fopen( "output.bin","wb");
+
+    if( !frp )
+    {
+        printf("Error: file not found!");
+        return -1;
+    }
+
+    for( u16ReadBytes = 2352; u16ReadBytes == 2352;  )
+    {
+        u16ReadBytes = fread( u8Sector, sizeof( U8 ), 2352, frp );
+        BinConvert_CDSector2352( u8Sector, u8PitsnLands );
+        fwrite( u8PitsnLands, sizeof( U8 ), 2352, fwp );
+    }
+
+    fclose( frp );
+    fclose( fwp );
+
+    return 0;
+}
